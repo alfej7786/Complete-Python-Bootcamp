66fb2cf94668a34505292f64c8ea6befe64e115e
Alfej Savaya <105317617+alfej7786@users.noreply.github.com>
2023-01-12 14:53:35 -0500

    dsa

--- Data Structure and Algorithm/01-BigO/bogO.py
+++ Data Structure and Algorithm/01-BigO/bogO.py
@@ -1,3 +1,4 @@
+
 # --- O(n) ---
 # def print_items(n):
 #     for i in range(n):
@@ -5,23 +6,36 @@
 
 # print_items(10)
 
-## --- Drop Contants ---
-def print_items(n):
-    for i in range(n):
-        print(i)
-        
-    for j in range(n):
-        print(j)
 
-print_items(10)
+## --- Drop Contants ---
+# def print_items(n):
+#     for i in range(n):
+#         print(i)
+        
+#     for j in range(n):
+#         print(j)
+
+# print_items(10)
 
 
 ## --- Squared ---
-def print_items(n):
-    for i in range(n):
-        for j in range(n):
-            # print(i,j) # n*n = n^2
-            for k in range(n):
-                print(i,j,k) # 0(n^2)
+# def print_items(n):
+#     for i in range(n):
+#         for j in range(n):
+#             # print(i,j) # n*n = n^2
+#             for k in range(n):
+#                 print(i,j,k) # O(n^2)
 
-print_items(10)
+# print_items(10)
+
+
+## --- Drop-Non Dominants ---
+# def print_items(n):
+#     for i in range(n):
+#         for j in range(n):
+#             print(i,j) # n*n = n^2
+            
+#     for k in range(n):
+#         print(k) # O(n^2)
+
+# print_items(10) # O(n^2 + n)
