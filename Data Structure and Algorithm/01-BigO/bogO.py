398aeb9696192e37e68a98ab8b3b9ec302d437b1
Alfej Savaya <105317617+alfej7786@users.noreply.github.com>
2023-01-12 14:45:55 -0500

    DSA

---
+++ Data Structure and Algorithm/01-BigO/bogO.py
@@ -0,0 +1,16 @@
+# O(n)
+# def print_items(n):
+#     for i in range(n):
+#         print(i)
+
+# print_items(10)
+
+# Drop Contants
+def print_items(n):
+    for i in range(n):
+        print(i)
+        
+    for j in range(n):
+        print(j)
+
+print_items(10)
