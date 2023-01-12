987b736962e5b25539f3ada1661b0158503e8052
Alfej Savaya <105317617+alfej7786@users.noreply.github.com>
2023-01-12 15:16:43 -0500

    classes and pointers

---
+++ Data Structure and Algorithm/02-Classes_and_Pointers/pointers.py
@@ -0,0 +1,20 @@
+# --- POINTERS --- #
+num1 = 11
+num2 = num1
+
+print(num1)
+print(num2)
+
+print(id(num1))
+print(id(num2))
+
+
+########
+num1 = 22
+num2 = num1
+
+print('\n',num1)
+print(num2)
+
+print(id(num1))
+print(id(num2))
