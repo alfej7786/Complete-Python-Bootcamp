7edcdda5f11757d6c815fdf2fd8f41fcb8052240
Alfej Savaya <105317617+alfej7786@users.noreply.github.com>
2023-01-14 13:16:39 -0500

    append

--- Data Structure and Algorithm/03-Linked-List/EXERCISE-LL-Append.py
+++ Data Structure and Algorithm/03-Linked-List/EXERCISE-LL-Append.py
@@ -29,6 +29,12 @@ class LinkedList:
         # FINISH WRITING APPEND METHOD  #
         # INSERT IF/ELSE STATEMENT HERE #
         #################################
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
         
         self.length += 1
 
